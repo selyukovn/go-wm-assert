0c530f4ea87f9e5128f88a75567b77a33d3838e4
Nikita Selyukov <selyukovn@yandex.ru>
2025-11-11 22:56:39 +0300

    Fix : changed MixinCustom value type parameter to 'any' from 'comparable'

--- b_mix_custom.go
+++ b_mix_custom.go
@@ -1,10 +1,10 @@
 package assert
 
-type mixinCustom[A assertInterface[T], T comparable] struct {
+type mixinCustom[A assertInterface[T], T any] struct {
 	assert A
 }
 
-func newMixinCustom[A assertInterface[T], T comparable](assert A) *mixinCustom[A, T] {
+func newMixinCustom[A assertInterface[T], T any](assert A) *mixinCustom[A, T] {
 	return &mixinCustom[A, T]{assert: assert}
 }
 
